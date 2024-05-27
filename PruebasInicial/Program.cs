0d799df6e43d2374b5d8bb054aaf335a0fb0c40b
Profesor <Profesor@DESKTOP-KS5R5DA>
2024-05-27 12:47:54 +0200

    Terminado

--- PruebasInicial/Program.cs
+++ PruebasInicial/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ICalculadora, CalculadoraClasica>();
+builder.Services.AddScoped<IConcatenador, ConcatenadorUno>();
 
 var app = builder.Build();
 
